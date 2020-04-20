3e2d6aeb6b987e08d1d28beebc6b0e58ed46db49
千珏 <admin@dowte.com>
2020-04-20 09:58:00 +0800

    test update

--- buffile_reader_test.go
+++ buffile_reader_test.go
@@ -1,10 +1,12 @@
 package buf_file
 
 import (
+	"bufio"
 	"io"
 	"io/ioutil"
 	"math/rand"
 	"os"
+	"sync"
 	"testing"
 	"time"
 )
@@ -40,12 +42,71 @@ func TestBufFileReader_Close(t *testing.T) {
 }
 
 func TestBufFileReader_ReadAndWrite(t *testing.T) {
-	WriteAndManyReader(t, 10, 1222166, 333343)
+	e := &E{t: t}
+	WriteAndManyReaderRandomString(e, 10, 1222166, 333343)
 }
 
 func TestBufFileReader_ReadAndWriteLessBuffSize(t *testing.T) {
+	e := &E{t: t}
 	// 77 char * 1222166 ~ 89M
-	WriteAndManyReader(t, 10, 1222166, 133)
+	WriteAndManyReaderRandomString(e, 10, 1222166, 133)
+}
+
+func BenchmarkBufFileReader_ReadAndWriteWithManyReader(b *testing.B) {
+	e := &E{b: b}
+
+	WriteAndManyReaderRandomString(e, 10, b.N, 1024*1024*4)
+}
+
+// only write
+func BenchmarkBufFileReader_ReadAndWrite(b *testing.B) {
+	tempFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		b.Error(err)
+	}
+
+	bufFile, err := NewBufFile(tempFile.Name(), 1024*1024*4)
+
+	if err != nil {
+		b.Error(err)
+	}
+
+	writer, err := bufFile.GetWriter()
+
+	if err != nil {
+		b.Error(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		writer.Write(s)
+	}
+	writer.Close()
+
+	tempFile.Close()
+	os.Remove(tempFile.Name())
+}
+
+// 1000000	      1172 ns/op
+func BenchmarkBufFileReader_ReadAndWriteSimpleWithManyReader(b *testing.B) {
+	e := &E{b: b}
+	WriteAndManyReaderSimple(e, 20, b.N, 1024*1024*4)
+}
+
+func BenchmarkBufioWrite(b *testing.B) {
+	tempFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		b.Error(err)
+	}
+
+	writer := bufio.NewWriterSize(tempFile, 1024*1024*4)
+
+	for i := 0; i < b.N; i++ {
+		writer.Write(s)
+	}
+	writer.Flush()
+
+	tempFile.Close()
+	os.Remove(tempFile.Name())
 }
 
 func readFileWithReadAt(read io.ReaderAt, total int) []byte {
@@ -69,7 +130,7 @@ func readFileWithReadAt(read io.ReaderAt, total int) []byte {
 	return readContent
 }
 
-func WriteAndManyReader(t *testing.T, readerCount int, writeCount int, bufSize int) {
+func WriteAndManyReaderRandomString(t *E, readerCount int, writeCount int, bufSize int) {
 	tempFile, err := ioutil.TempFile("", "")
 	if err != nil {
 		t.Error(err)
@@ -86,40 +147,109 @@ func WriteAndManyReader(t *testing.T, readerCount int, writeCount int, bufSize i
 	n := writeCount
 	var writeContent []byte
 
+	wg := &sync.WaitGroup{}
 	go func() {
+		wg.Add(1)
 		for i := 0; i < n; i++ {
 			str := []byte(RandomString(77))
 			writeContent = append(writeContent, str...)
 			writer.Write(str)
 		}
 		writer.Close()
+		wg.Done()
 	}()
 
 	for i := 0; i < readerCount-1; i++ {
+		wg.Add(1)
 		go func() {
 			reader, err := bufFile.GetReader()
 			if err != nil {
 				t.Error(err)
+				return
 			}
 			data := readFileWithReadAt(reader, 77*n)
 			if hashx(data) != hashx(writeContent) {
 				t.Error(err)
 			}
 			reader.Close()
+			wg.Done()
 		}()
 	}
 
-	reader, err := bufFile.GetReader()
+	if readerCount > 0 {
+		reader, err := bufFile.GetReader()
+		if err != nil {
+			t.Error(err)
+		}
+
+		data := readFileWithReadAt(reader, 77*n)
+
+		if hashx(data) != hashx(writeContent) {
+			t.Error()
+		}
+		reader.Close()
+	}
+
+	wg.Wait()
+
+	tempFile.Close()
+	os.Remove(tempFile.Name())
+
+	return
+}
+
+func WriteAndManyReaderSimple(t *E, readerCount int, writeCount int, bufSize int) {
+	tempFile, err := ioutil.TempFile("", "")
 	if err != nil {
 		t.Error(err)
 	}
 
-	data := readFileWithReadAt(reader, 77*n)
+	bufFile, err := NewBufFile(tempFile.Name(), bufSize)
 
-	if hashx(data) != hashx(writeContent) {
-		t.Error()
+	if err != nil {
+		t.Error(err)
 	}
-	reader.Close()
+
+	writer, err := bufFile.GetWriter()
+
+	n := writeCount
+
+	wg := &sync.WaitGroup{}
+	go func() {
+		wg.Add(1)
+		for i := 0; i < n; i++ {
+			writer.Write(s)
+		}
+		writer.Close()
+		wg.Done()
+	}()
+
+	for i := 0; i < readerCount-1; i++ {
+		wg.Add(1)
+		go func() {
+			reader, err := bufFile.GetReader()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			readFileWithReadAt(reader, 64*n)
+
+			reader.Close()
+			wg.Done()
+		}()
+	}
+
+	if readerCount > 0 {
+		reader, err := bufFile.GetReader()
+		if err != nil {
+			t.Error(err)
+		}
+
+		readFileWithReadAt(reader, 64*n)
+		reader.Close()
+	}
+
+	wg.Wait()
 
 	tempFile.Close()
 	os.Remove(tempFile.Name())
@@ -141,4 +271,17 @@ func StringWithCharset(length int, charset string) string {
 
 func RandomString(length int) string {
 	return StringWithCharset(length, charset)
+}
+
+type E struct {
+	b *testing.B
+	t *testing.T
+}
+
+func (e E) Error(args ...interface{}) {
+	if e.b != nil {
+		e.b.Error(args...)
+	} else {
+		e.t.Error(args...)
+	}
 }
